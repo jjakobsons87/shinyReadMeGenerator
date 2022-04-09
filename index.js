c96f80a4adfbba610000cb3122ecee4b0a69f55d
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-09 11:10:02 -0400

    additional questions and verify we are asking questions

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateFile = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         name: 'title',
@@ -134,13 +136,16 @@ const questions = [
             }
         } 
     },
-];
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+questions(); 
